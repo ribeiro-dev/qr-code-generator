51441762ea79e1d7c772270b1f83ec29d02d15ff
ribeiro-dev <romilson.2r@gmail.com>
2023-04-02 20:42:32 -0300

    add: generator working

--- scripts.js
+++ scripts.js
@@ -1,1 +1,27 @@
+const btnGenerate = document.querySelector('.btn-generate')
+const input = document.querySelector('.input')
 const resultDiv = document.querySelector('.result')
+const resultImg = document.querySelector('.qr-code')
+
+const imgSize = '200x200'
+const baseURL = `https://api.qrserver.com/v1/create-qr-code/?size=${imgSize}`
+
+
+btnGenerate.addEventListener('click', async (event) => {
+    event.preventDefault()
+    const URL = baseURL + `&data=${input.value}`
+    
+    try {
+        const response = await fetch(URL)
+
+        if (response.ok) {
+            resultImg.src = response.url
+            return
+        }
+        throw new Error('Error while fetching')
+
+    } catch (error) {
+        resultDiv.innerHTML = "Infelizmente ocorreu um erro :("
+    }
+
+})
