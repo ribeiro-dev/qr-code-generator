75ea0d138f8f7c199f487fad3e69cd18e8a7fc17
ribeiro-dev <romilson.2r@gmail.com>
2023-04-26 10:55:42 -0300

    refactor: change logic of loading image display

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@ const input = document.querySelector('.input')
 const resultDiv = document.querySelector('.result')
 const resultAlert = document.querySelector('.alerts-wrapper')
 const resultImg = document.querySelector('.qr-code')
+const loadingImg = document.querySelector('.loading')
 const form = document.forms.urlText
 
 const imgSize = '200x200'
@@ -13,24 +14,22 @@ const baseURL = `https://api.qrserver.com/v1/create-qr-code/?size=${imgSize}`
 form.addEventListener('submit', async (event) => {
     event.preventDefault()
     
-    const inputValue = input.value.trim()
+    loadingImg.style.display = 'inline'
 
+    const inputValue = input.value.trim()
     const URL = baseURL + `&data=${inputValue}`
 
+    // resets the contents every submit
     resultAlert.innerHTML = ''
     resultImg.src = ''
-
-    resultImg.src = 'imgs/reload.png'
-    resultImg.classList.toggle('loading')
     
     try {
         const response = await fetch(URL)
+        loadingImg.style.display = 'none'
 
         if (response.ok) {
             resultAlert.innerHTML = `<p class="alert alert-success d-inline-flex">Sucesso!</p>`
-
             resultImg.src = response.url
-            resultImg.classList.toggle('loading')
 
             return
         }
@@ -38,10 +37,6 @@ form.addEventListener('submit', async (event) => {
 
     } catch (error) {
         resultAlert.innerHTML = `<p class="alert alert-danger d-inline-flex">Infelizmente ocorreu um erro :(</p>`
-        resultImg.classList.toggle('loading')
-        resultImg.src = ''
-
         console.log(error)
     }
-
 })
